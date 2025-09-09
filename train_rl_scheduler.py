f1885add082007e31fa174237ad2ba6027e38530
Phạm Anh Tuấn <pat25bk@gmail.com>
2025-09-09 11:21:07 +0700

    Update train_rl_scheduler.py

--- train_rl_scheduler.py
+++ train_rl_scheduler.py
@@ -61,6 +61,8 @@ def main():
                 n_steps=args.n_steps,
                 batch_size=args.batch_size,
                 ent_coef=args.ent_coef,
+                vf_coef=1,
+                n_epochs=10,
                 gamma=0.99, gae_lambda=0.95, clip_range=0.2, vf_coef=0.5,
                 policy_kwargs=policy_kwargs, verbose=1, seed=args.seed)
     model.set_logger(logger)
